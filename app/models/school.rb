e254aac7aad8711722c30376f20afab45493f706
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-22 13:32:21 +0530

    Added required validations to School and Student

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,4 @@
 class School < ApplicationRecord
+  validates_presence_of :name
+  validates :name, uniqueness: { message: "School Already Registered" }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+  validates_presence_of :name, :student_number
+  validates :student_number, uniqueness: {message: "Student Id Already Exists.."}
 end
